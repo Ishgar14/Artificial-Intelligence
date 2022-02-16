130dad36dbe18abf156fced0c8374c120e20ed2e
Rushikesh <rushikeshjyoti14@gmail.com>
2022-02-16 15:30:01 +0530

    changed minimax_tictactoe file to improve intelligence of bot

--- Assignments/Ass1/minimax_tictactoe.py
+++ Assignments/Ass1/minimax_tictactoe.py
@@ -1,176 +1,195 @@
-EMPTY = ' '
 PLAYER_CHARACTER = 'O'
 COMPUTER_CHARACTER = 'X'
 
-
-def display_board(board: dict[int, str]) -> None:
-    vals = []
-    for i, val in board.items():
-        if val == ' ':
-            vals.append(str(i))
-        else:
-            vals.append(val)
-
-    vals = [val.center(3, ' ') for val in vals]
-
-    print('|'.join(vals[:3]))
-    print('-' * 10)
-    print('|'.join(vals[3:6]))
-    print('-' * 10)
-    print('|'.join(vals[6:]))
+board = {
+    1: ' ', 2: ' ', 3: ' ',
+    4: ' ', 5: ' ', 6: ' ',
+    7: ' ', 8: ' ', 9: ' '
+}
 
 
-def has_empty_slots(board: dict[int, str]) -> bool:
-    return ' ' in board.values()
+def display_board(board):
+    print(
+        board[1].center(5, ' ') + '|' + 
+        board[2].center(5, ' ') + '|' + 
+        board[3].center(5, ' ')
+    )
+    print('-' * 17)
+    print(
+        board[4].center(5, ' ') + '|' + 
+        board[5].center(5, ' ') + '|' + 
+        board[6].center(5, ' ')
+    )
+    print('-' * 17)
+    print(
+        board[7].center(5, ' ') + '|' + 
+        board[8].center(5, ' ') + '|' + 
+        board[9].center(5, ' ')
+    )
+    print('\n')
 
 
-def is_winner(board: dict[int, str], mark: str) -> bool:
+def is_free(pos):
+    return board[pos] == ' '
+
+
+def is_draw():
+    for key in board.keys():
+        if board[key] == ' ':
+            return False
+    return True
+
+
+def somebody_won():
     # Row check
-    if board[1] == board[2] == board[3] and board[1] == mark:
+    if board[1] == board[2] and board[1] == board[3] and board[1] != ' ':
         return True
-    elif board[4] == board[5] == board[6] and board[4] == mark:
+    elif board[4] == board[5] and board[4] == board[6] and board[4] != ' ':
         return True
-    elif board[7] == board[8] == board[9] and board[7] == mark:
+    elif board[7] == board[8] and board[7] == board[9] and board[7] != ' ':
         return True
-    
+
     # Column check
-    elif board[1] == board[4] == board[7] and board[1] == mark:
+    elif board[1] == board[4] and board[1] == board[7] and board[1] != ' ':
         return True
-    elif board[2] == board[5] == board[8] and board[2] == mark:
+    elif board[2] == board[5] and board[2] == board[8] and board[2] != ' ':
         return True
-    elif board[3] == board[6] == board[9] and board[3] == mark:
+    elif board[3] == board[6] and board[3] == board[9] and board[3] != ' ':
         return True
 
     # Diagonal check
-    elif board[1] == board[5] == board[9] and board[1] == mark:
+    elif board[1] == board[5] and board[1] == board[9] and board[1] != ' ':
         return True
-    elif board[7] == board[5] == board[3] and board[7] == mark:
+    elif board[7] == board[5] and board[7] == board[3] and board[7] != ' ':
         return True
-    
-    return False
-
-def has_winner(board: dict[int, str]) -> bool:
-    return is_winner(board, PLAYER_CHARACTER) or is_winner(board, COMPUTER_CHARACTER)
+    else:
+        return False
 
 
-def game_status(board, char: str):
-    if has_winner(board):
-        if char == COMPUTER_CHARACTER:
-            print("\nThe computer wins 💻")
-        else:
-            print("\nThe player wins 🤴")
+def has_won(mark):
+    # Row check
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif board[4] == board[5] and board[4] == board[6] and board[4] == mark:
+        return True
+    elif board[7] == board[8] and board[7] == board[9] and board[7] == mark:
+        return True
+
+    # Column check
+    elif board[1] == board[4] and board[1] == board[7] and board[1] == mark:
+        return True
+    elif board[2] == board[5] and board[2] == board[8] and board[2] == mark:
+        return True
+    elif board[3] == board[6] and board[3] == board[9] and board[3] == mark:
+        return True
+
+    # Diagoanl check
+    elif board[1] == board[5] and board[1] == board[9] and board[1] == mark:
+        return True
+    elif board[7] == board[5] and board[7] == board[3] and board[7] == mark:
+        return True
+    else:
+        return False
+
+
+def putchar(character, position):
+    if is_free(position):
+        board[position] = character
         display_board(board)
-        exit()
 
-    if not has_empty_slots(board):
-        print("\nIt was a tie 👨🤝💻")
-        display_board(board)
-        exit()
+        if is_draw():
+            print("\nIt was a tie 👨🤝💻")
+            exit()
 
-def player_move(board) -> None:
-    print(f" Player's Turn [{PLAYER_CHARACTER}] ".center(40, '='))
-    display_board(board)
-    while True:
-        position = int(input("Enter the position: "))
-        if board[position] != EMPTY:
-            print("Position is already occupied")
-            continue
-        break
-    
-    board[position] = PLAYER_CHARACTER
+        if somebody_won():
+            if character == COMPUTER_CHARACTER:
+                print("\nThe computer wins 💻")
+            else:
+                print("\nThe player wins 🤴")
+            exit()
+
+    else:
+        print("This slot is already used!")
+        position = int(input("Enter new position: "))
+        putchar(character, position)
 
 
-def computer_move(board) -> None:
-    print(f" Computer's Turn [{COMPUTER_CHARACTER}] ".center(40, '='))
-    display_board(board)
-    bestScore = -1000
-    bestMove = 0
+def get_player_move():
+    print(" Player's Turn ".center(40, '='))
+    pos = int(input("Enter the position for 'O': "))
+    putchar(PLAYER_CHARACTER, pos)
 
-    for key in board:
-        if board[key] != EMPTY:
+
+def get_computer_move():
+    print(" Computer's Turn ".center(40, '='))
+    best_score = -100
+    best_move = 0
+
+    for key in board.keys():
+        if board[key] != ' ':
             continue
 
         board[key] = COMPUTER_CHARACTER
         score = minimax(board, False)
-        board[key] = EMPTY
-        
-        if(score > bestScore):
-            bestScore = score
-            bestMove = key
-            print(f"Computer found {bestMove} with score {bestScore}")
+        board[key] = ' '
 
-    board[bestMove] = COMPUTER_CHARACTER
-    print(f"Computer picked {bestMove} with score {bestScore}")
+        if score > best_score:
+            best_score = score
+            best_move = key
+            print(f"Computer found move {best_move} with score {best_score}")
 
-def heuristic(board: dict[int, str]) -> int:
-    if is_winner(board, COMPUTER_CHARACTER) :
+    print(f"Computer picks move {best_move} with score {best_score}")
+    putchar(COMPUTER_CHARACTER, best_move)
+
+
+def minimax(board, maximising):
+    if has_won(COMPUTER_CHARACTER):
         return 10
-    elif is_winner(board, PLAYER_CHARACTER):
+    if has_won(PLAYER_CHARACTER):
         return -10
-    elif has_empty_slots(board):
+    elif is_draw():
         return 0
 
-    return None
-    
-
-def minimax(board: dict[int, str], maximise: bool) -> int:
-    val = heuristic(board)
-    if val is not None:
-        return val
-
-    if maximise:
+    if maximising:
         bestScore = -1000
         for key in board.keys():
-            if board[key] != EMPTY:
-                continue
-            
-            board[key] = COMPUTER_CHARACTER
-            score = minimax(board, False)
-            board[key] = EMPTY
-            
-            if score > bestScore:
-                bestScore = score
+            if board[key] == ' ':
+
+                board[key] = COMPUTER_CHARACTER
+                score = minimax(board, False)
+                board[key] = ' '
+
+                if score > bestScore:
+                    bestScore = score
+
+        return bestScore
+
     else:
         bestScore = 1000
         for key in board.keys():
-            if board[key] != EMPTY:
-                continue
-            
-            board[key] = PLAYER_CHARACTER
-            score = minimax(board, True)
-            board[key] = EMPTY
-            
-            if score < bestScore:
-                bestScore = score
-    
-    return bestScore
+            if board[key] == ' ':
 
-def get_characters():
-    global PLAYER_CHARACTER, COMPUTER_CHARACTER
-    PLAYER_CHARACTER = input("Enter your character(X/O): ").strip().upper()
+                board[key] = PLAYER_CHARACTER
+                score = minimax(board, True)
+                board[key] = ' '
 
-    if PLAYER_CHARACTER == 'X':
-        COMPUTER_CHARACTER = 'O'
-    else:
-        COMPUTER_CHARACTER = 'X'
+                if score < bestScore:
+                    bestScore = score
 
-def main(board):
-    turn = PLAYER_CHARACTER != 'X'
-    while has_empty_slots(board) or not has_winner(board):
+        return bestScore
+
+
+def main():
+    display_board(board)
+    first = input("Do you wish to play first (Y/N): ").strip().upper()
+    turn = first == 'Y'
+
+    while not somebody_won():
         if turn:
-            computer_move(board)
-            game_status(board, COMPUTER_CHARACTER)
+            get_player_move()
         else:
-            player_move(board)
-            game_status(board, PLAYER_CHARACTER)
+            get_computer_move()
         turn = not turn
 
-
 if __name__ == '__main__':
-    board = {
-        1: ' ', 2: ' ', 3: ' ',
-        4: ' ', 5: ' ', 6: ' ',
-        7: ' ', 8: ' ', 9: ' '
-    }
-    get_characters()
-    main(board)
+    main()
