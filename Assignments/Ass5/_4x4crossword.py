886f7179a247ea1d206a8d4b11dcdd54a4434703
Rushikesh <rushikeshjyoti14@gmail.com>
2022-03-16 15:47:57 +0530

    rewrote the 4x4 crossword from scratch using trie

--- Assignments/Ass5/_4x4crossword.py
+++ Assignments/Ass5/_4x4crossword.py
@@ -16,59 +16,98 @@ def display_board(board: Board) -> None:
     for word in board:
         print(' '.join(word))
 
-
-def get_row_words(grid: Board) -> list[str]:
+def row_words(grid: Board) -> list[str]:
     return [row for row in grid]
 
-
-def get_col_words(grid: Board) -> list[str]:
+def col_words(grid: Board) -> list[str]:
     return [col_str(grid, i) for i in range(len(grid))]
 
+def all_words_of(grid: Board) -> list[str]:
+    words = row_words(grid)
+    words.extend(col_words(grid))
+    return words
 
-def get_all_words(grid: Board) -> list[str]:
-    return get_row_words(grid).extend(get_col_words(grid))
+def generate_trie(words: list[str]) -> dict:
+    root = {}
+    for word in words:
+        current = root
+        for letter in word:
+            current[letter] = current.get(letter, {})
+            current = current[letter]
+
+    return root
 
 
-def vertical_bucket_exists(size: int, words: list[str], grid: Board) -> bool:
-    existence = []
-    for i in range(size):
-        existence.append(
-            any(filter(lambda word: word.startswith(col_str(grid, i)), words)))
+def vertical_bucket_exists(trie: dict, grid: Board) -> bool:
+    column_words = [col_str(grid, i) for i in range(4)]
 
-    return all(existence)
+    for word in column_words:
+        current = trie
+        for letter in word:
+            current = current.get(letter, None)
+            if current is None:
+                return False
+
+    return True
 
 
-def start(size: int, words: list[str], grid: Board = [], max_count: int = 50) -> Board:
+def word_exists(word: str, trie: dict) -> bool:
+    current = trie
+
+    for letter in word:
+        current = current.get(letter, None)
+        if current is None:
+            return False
+
+    return True
+
+
+def start(size: int, words: list[str], trie: dict, grid: Board = [], 
+        filter_duplicate_words: bool = False, max_count: int = 50) -> Board:
     # Pick the next word
     # check if vertical buckets for that word are not empty
     # if any bucket is empty then backtrack
     # else proceed further
 
+    if len(grid) == size:
+        column_words = [col_str(grid, i) for i in range(size)]
+        column_words_exist = [
+            word_exists(word, trie)
+            for word in column_words
+        ]
+
+        if not all(column_words_exist):
+            grid.pop()
+            return None
+
+        return grid
+
     for word in words:
         grid.append(word)
 
-        if len(grid) != size and not vertical_bucket_exists(size, words, grid):
+        if not vertical_bucket_exists(trie, grid):
             grid.pop()
             continue
 
-        if len(grid) == size:
-            column_words = [col_str(grid, i) for i in range(len(grid))]
-            column_words_exist = [word in words for word in column_words]
+        if len(RESULTS) >= max_count:
+            return None
 
-            if not all(column_words_exist):
+        if filter_duplicate_words:
+            all_words = all_words_of(grid)
+            if len(all_words) != len(set(all_words)):
                 grid.pop()
                 continue
 
-            RESULTS.append(grid.copy())
-            grid.clear()
+        if val := start(size, words, trie, grid):
+            if val not in RESULTS:
+                RESULTS.append(val.copy())
+                grid.pop()
 
-            if len(RESULTS) >= max_count:
-                return
 
 
 def main() -> None:
     words = load_file()
-    start(4, words)
+    start(4, words, generate_trie(words))
 
     if len(RESULTS) == 0:
         print("Could not find any such combination")
@@ -81,3 +120,6 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    # print(word_exists("goat", load_file()) + 1)
+    # from pprint import pprint
+    # print(generate_trie(load_file()))
